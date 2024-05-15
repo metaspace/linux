247fa487fbcc1c2a73912a08d9dba7c6eb23cc55
Andreas Hindborg <a.hindborg@samsung.com>
2024-05-15 13:44:11 -0600

    rust: block: add `TagSet` private data support

--- drivers/block/rnull/rnull.rs
+++ drivers/block/rnull/rnull.rs
@@ -186,7 +186,7 @@ impl NullBlkDevice {
         }
 
         let tagset = Arc::pin_init(
-            TagSet::new(submit_queues, 256, 1, home_node, flags),
+            TagSet::new(submit_queues, (), 256, 1, home_node, flags),
             GFP_KERNEL,
         )?;
 
@@ -513,6 +513,7 @@ where
 impl Operations for NullBlkDevice {
     type QueueData = Pin<KBox<QueueData>>;
     type RequestData = Pdu;
+    type TagSetData = ();
 
     fn new_request_data() -> impl PinInit<Self::RequestData> {
         pin_init!(Pdu {
