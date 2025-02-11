6f4e010f120e0508a94fa209c5e4251390f59b48
Andreas Hindborg <a.hindborg@kernel.org>
2025-02-11 13:20:04 +0100

    block: rnull: allow specifying the home numa node
    Add a configfs attribute to specify the NUMA node for rnull tag set
    and CPU map allocations. This allows testing NUMA-aware block device
    behavior and optimizing memory placement for specific hardware
    configurations.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- drivers/block/rnull/rnull.rs
+++ drivers/block/rnull/rnull.rs
@@ -20,7 +20,10 @@ use kernel::{
             TagSet, //
         },
     },
-    error::Result,
+    error::{
+        code,
+        Result, //
+    },
     new_mutex,
     new_xarray,
     page::SafePage,
@@ -95,6 +98,10 @@ module! {
             description:
         "Use per-node allocation for hardware context queues, 0-false, 1-true. Default: 0-false",
         },
+        home_node: i32 {
+            default: -1,
+            description: "Home node for the device. Default: -1 (no node)",
+        },
     },
 }
 
@@ -131,6 +138,7 @@ impl kernel::InPlaceModule for NullBlkModule {
                     completion_time: Delta::from_nanos(completion_time),
                     memory_backed: *module_parameters::memory_backed.value() != 0,
                     submit_queues,
+                    home_node: *module_parameters::home_node.value(),
                 })?;
                 disks.push(disk, GFP_KERNEL)?;
             }
@@ -154,6 +162,7 @@ struct NullBlkOptions<'a> {
     completion_time: Delta,
     memory_backed: bool,
     submit_queues: u32,
+    home_node: i32,
 }
 struct NullBlkDevice;
 
@@ -168,6 +177,7 @@ impl NullBlkDevice {
             completion_time,
             memory_backed,
             submit_queues,
+            home_node,
         } = options;
 
         let flags = if memory_backed {
@@ -176,8 +186,12 @@ impl NullBlkDevice {
             mq::tag_set::Flags::default()
         };
 
+        if home_node > kernel::num_online_nodes().try_into()? {
+            return Err(code::EINVAL);
+        }
+
         let tagset = Arc::pin_init(
-            TagSet::new(submit_queues, 256, 1, bindings::NUMA_NO_NODE, flags),
+            TagSet::new(submit_queues, 256, 1, home_node, flags),
             GFP_KERNEL,
         )?;
 
