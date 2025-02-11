7389dcc384181e692ae572f4bc01928d8802e844
Andreas Hindborg <a.hindborg@kernel.org>
2025-02-11 12:59:38 +0100

    rust: block: allow specifying home node when constructing `TagSet`
    Allow specifying the home numa node when allocating a new `TagSet`. The home
    node will be used when allocating memory for list of tag sets and for the cpu
    map structures.

--- drivers/block/rnull/rnull.rs
+++ drivers/block/rnull/rnull.rs
@@ -146,7 +146,10 @@ impl NullBlkDevice {
             mq::Flags::default()
         };
 
-        let tagset = Arc::pin_init(TagSet::new(submit_queues, 256, 1, flags), GFP_KERNEL)?;
+        let tagset = Arc::pin_init(
+            TagSet::new(submit_queues, 256, 1, bindings::NUMA_NO_NODE, flags),
+            GFP_KERNEL,
+        )?;
 
         let queue_data = Box::pin_init(
             pin_init!(
