4b2e16004e4170c4e992244b49f0859257711dec
Gary Guo <gary@garyguo.net>
2025-02-19 20:15:30 +0000

    LIST: [PATCH v3 1/4] rust: implement `kernel::sync::Refcount`
    This is a wrapping layer of `include/linux/refcount.h`. Currently the
    kernel refcount has already been used in `Arc`, however it calls into
    FFI directly.
    
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Reviewed-by: Boqun Feng <boqun.feng@gmail.com>
    Signed-off-by: Gary Guo <gary@garyguo.net>
    Reviewed-by: Fiona Behrens <me@kloenk.dev>
    Link: https://lore.kernel.org/r/20250219201602.1898383-2-gary@garyguo.net

--- rust/kernel/sync.rs
+++ rust/kernel/sync.rs
@@ -13,6 +13,7 @@ pub mod lock;
 mod locked_by;
 pub mod poll;
 pub mod rcu;
+mod refcount;
 
 pub use arc::{Arc, ArcBorrow, UniqueArc};
 pub use condvar::{new_condvar, CondVar, CondVarTimeoutResult};
@@ -20,6 +21,7 @@ pub use lock::global::{global_lock, GlobalGuard, GlobalLock, GlobalLockBackend, 
 pub use lock::mutex::{new_mutex, Mutex, MutexGuard};
 pub use lock::spinlock::{new_spinlock, SpinLock, SpinLockGuard};
 pub use locked_by::LockedBy;
+pub use refcount::Refcount;
 
 /// Represents a lockdep class. It's a wrapper around C's `lock_class_key`.
 #[repr(transparent)]
