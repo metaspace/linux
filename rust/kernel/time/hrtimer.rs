e3b9c9908688c180f223632fb56f99434f29baef
Andreas Hindborg <a.hindborg@kernel.org>
2025-08-21 11:47:45 +0200

    block: rust: allow `hrtimer::Timer` in `RequestData`
    `Request` is essentially a smart pointer that derefs to
    `Operations::RequestData`. To use an `HrTimer` in `Operations::RequestData`
    via the `Request` pointer, we must implement `HrTimerPointer` for
    `Request`.
    
    Thus, implement `HrTimerPointer` and friends for `ARef<Request>`.
    
    Publicly export `HrTimer::raw_cancel` and `HrTimer::into_c`.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- rust/kernel/time/hrtimer.rs
+++ rust/kernel/time/hrtimer.rs
@@ -160,7 +160,7 @@ impl<T> HrTimer<T> {
     /// # Safety
     ///
     /// `this` must point to a valid `Self`.
-    pub(crate) unsafe fn raw_cancel(this: *const Self) -> bool {
+    pub unsafe fn raw_cancel(this: *const Self) -> bool {
         // SAFETY: `this` points to an allocation of at least `HrTimer` size.
         let c_timer_ptr = unsafe { HrTimer::raw_get(this) };
 
@@ -514,7 +514,8 @@ pub enum HrTimerRestart {
 }
 
 impl HrTimerRestart {
-    fn into_c(self) -> bindings::hrtimer_restart {
+    /// Convert `self` into an integer for FFI use.
+    pub fn into_c(self) -> bindings::hrtimer_restart {
         self as bindings::hrtimer_restart
     }
 }
