488e7ab35c1787fdff92560e4a28403f8a98e1b2
Andreas Hindborg <a.hindborg@kernel.org>
2025-08-21 12:02:44 +0200

    block: rust: add `TagSet` flags
    Add support for `TagSet` flags by introducing a `Flags` type and adding
    a flags parameter to `TagSet::new`. This allows configuring tagset
    behavior such as blocking vs non-blocking operation.
    
    The Flags type supports bitwise operations and provides values like
    `Blocking` for common use cases. The module documentation example is
    updated to demonstrate the new API.
    
    For now, only a single flag is added.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- drivers/block/rnull/rnull.rs
+++ drivers/block/rnull/rnull.rs
@@ -130,7 +130,10 @@ impl NullBlkDevice {
         irq_mode: IRQMode,
         completion_time: Delta,
     ) -> Result<GenDisk<Self>> {
-        let tagset = Arc::pin_init(TagSet::new(1, 256, 1), GFP_KERNEL)?;
+        let tagset = Arc::pin_init(
+            TagSet::new(1, 256, 1, mq::tag_set::Flags::default()),
+            GFP_KERNEL,
+        )?;
 
         let queue_data = Box::new(
             QueueData {
