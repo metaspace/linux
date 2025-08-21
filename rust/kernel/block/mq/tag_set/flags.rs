c9565b60b9ec56d4ce445067d1003065355a499a
Andreas Hindborg <a.hindborg@kernel.org>
2025-08-21 12:02:44 +0200

    rust: block: add `TagSet` flags
    Add support for `TagSet` flags by introducing a `Flags` type and adding
    a flags parameter to `TagSet::new`. This allows configuring tagset
    behavior such as blocking vs non-blocking operation.
    
    The Flags type supports bitwise operations and provides constants
    like BLOCKING for common use cases. The module documentation example
    is updated to demonstrate the new API.
    
    For now, only a single flag is added.
    
    The API is based on irq flags work [1].
    
    Link: https://lore.kernel.org/r/20250811-topics-tyr-request_irq2-v9-2-0485dcd9bcbf@collabora.com
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

---
+++ rust/kernel/block/mq/tag_set/flags.rs
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-2.0
+
+use core::default::Default;
+use kernel::ffi::c_uint;
+
+/// Flags to be used when creating [`super::TagSet`] objects.
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub struct Flags(c_uint);
+
+impl Flags {
+    /// Indicate that the queues associated with this tag set might sleep when
+    /// processing IO. When this flag is not set, IO is processed in atomic
+    /// context. When this flag is set, IO is processed in process context.
+    pub const BLOCKING: Flags = Flags::new(bindings::BLK_MQ_F_BLOCKING);
+
+    pub(crate) fn into_inner(self) -> c_uint {
+        self.0
+    }
+
+    const fn new(value: u32) -> Self {
+        Self(value)
+    }
+}
+
+impl Default for Flags {
+    fn default() -> Self {
+        Self::new(0)
+    }
+}
+
+impl core::ops::BitOr for Flags {
+    type Output = Self;
+    fn bitor(self, rhs: Self) -> Self::Output {
+        Self(self.0 | rhs.0)
+    }
+}
+
+impl core::ops::BitAnd for Flags {
+    type Output = Self;
+    fn bitand(self, rhs: Self) -> Self::Output {
+        Self(self.0 & rhs.0)
+    }
+}
+
+impl core::ops::Not for Flags {
+    type Output = Self;
+    fn not(self) -> Self::Output {
+        Self(!self.0)
+    }
+}
