kernelorg-mirror/linux_kernel_git_a.hindborg_linux

rnull: add memory backing

drivers/block/rnull/configfs.rs
@@ -36,7 +36,7 @@ impl AttributeOperations<0> for Config {
 
     fn show(_this: &Config, page: &mut [u8; PAGE_SIZE]) -> Result<usize> {
         let mut writer = kernel::str::BufferWriter::new(page)?;
-        writer.write_str("blocksize,size,rotational,irqmode,completion_nsec\n")?;
+        writer.write_str("blocksize,size,rotational,irqmode,completion_nsec,memory_backed\n")?;
         Ok(writer.pos())
     }
 }
@@ -61,6 +61,7 @@ impl configfs::GroupOperations for Config {
                 size: 3,
                 irqmode: 4,
                 completion_nsec: 5,
+                memory_backed: 6,
             ],
         };
 
@@ -78,6 +79,7 @@ impl configfs::GroupOperations for Config {
                     irq_mode: IRQMode::None,
                     completion_time: Ktime::from_nanos(0),
                     name: name.try_into()?,
+                    memory_backed: false,
                 }),
             }),
         ))
@@ -131,6 +133,7 @@ struct DeviceConfigInner {
     irq_mode: IRQMode,
     completion_time: Ktime,
     disk: Option<GenDisk<NullBlkDevice>>,
+    memory_backed: bool,
 }
 
 #[vtable]
@@ -166,6 +169,7 @@ impl configfs::AttributeOperations<0> for DeviceConfig {
                 guard.capacity_mib,
                 guard.irq_mode,
                 guard.completion_time,
+                guard.memory_backed,
             )?);
             guard.powered = true;
         } else if guard.powered && !power_op {
@@ -287,6 +291,33 @@ impl configfs::AttributeOperations<5> for DeviceConfig {
         let completion_time: i64 = value.try_into()?;
 
         this.data.lock().completion_time = Ktime::from_nanos(completion_time);
+        Ok(())
+    }
+}
+
+#[vtable]
+impl configfs::AttributeOperations<6> for DeviceConfig {
+    type Data = DeviceConfig;
+
+    fn show(this: &DeviceConfig, page: &mut [u8; PAGE_SIZE]) -> Result<usize> {
+        let mut writer = kernel::str::BufferWriter::new(page)?;
+
+        if this.data.lock().memory_backed {
+            writer.write_fmt(fmt!("1\n"))?;
+        } else {
+            writer.write_fmt(fmt!("0\n"))?;
+        }
+
+        Ok(writer.pos())
+    }
+
+    fn store(this: &DeviceConfig, page: &[u8]) -> Result {
+        this.data.lock().memory_backed = core::str::from_utf8(page)?
+            .trim()
+            .parse::<u8>()
+            .map_err(|_| kernel::error::code::EINVAL)?
+            != 0;
+
         Ok(())
     }
 }

drivers/block/rnull/rnull.rs
@@ -13,23 +13,32 @@
 mod configfs;
 
 use configfs::IRQMode;
+use core::ops::Deref;
 use kernel::{
     alloc::{flags, KVec},
-    block::mq::{
-        self,
-        gen_disk::{self, GenDisk},
-        Operations, TagSet,
+    bindings,
+    block::{
+        bio::Segment,
+        mq::{
+            self,
+            gen_disk::{self, GenDisk},
+            Operations, TagSet,
+        },
     },
     error::Result,
-    new_mutex, pr_info,
+    new_mutex, new_spinlock,
+    page::Page,
+    pr_info,
     prelude::*,
     str::CString,
-    sync::{Arc, Mutex},
+    sync::{Arc, Mutex, SpinLock},
     time::{
         hrtimer::{HrTimerCallback, HrTimerPointer, HrTimerRestart},
         Ktime,
     },
     types::ARef,
+    xarray::XArray,
+    CacheAligned,
 };
 
 module! {
@@ -63,6 +72,10 @@ module! {
             default: 10_000,
             description:  "Time in ns to complete a request in hardware. Default: 10,000ns",
         },
+        memory_backed: u8 {
+            default: 0,
+            description: "Create a memory-backed block device. 0-false, 1-true. Default: 0",
+        },
     },
 }
 
@@ -91,6 +104,7 @@ impl kernel::InPlaceModule for NullBlkModule {
                     *module_parameters::gb.get(),
                     (*module_parameters::irqmode.get()).try_into()?,
                     Ktime::from_nanos(completion_time),
+                    *module_parameters::memory_backed.get() != 0,
                 )?;
                 disks.push(disk, flags::GFP_KERNEL)?;
             }
@@ -115,14 +129,18 @@ impl NullBlkDevice {
         capacity_mib: u64,
         irq_mode: IRQMode,
         completion_time: Ktime,
+        memory_backed: bool,
     ) -> Result<GenDisk<Self>> {
         let tagset = Arc::pin_init(TagSet::new(1, 256, 1), flags::GFP_KERNEL)?;
 
-        let queue_data = Box::new(
+        let queue_data = Box::pin_init(
+            pin_init!(
             QueueData {
+                tree <- TreeContainer::new(),
                 irq_mode,
                 completion_time,
-            },
+                memory_backed,
+            }),
             flags::GFP_KERNEL,
         )?;
 
@@ -133,11 +151,100 @@ impl NullBlkDevice {
             .rotational(rotational)
             .build(fmt!("{}", name.to_str()?), tagset, queue_data)
     }
+
+    #[inline(always)]
+    fn write(tree: TreeRef<'_>, sector: usize, segment: &Segment<'_>) -> Result {
+        let idx = sector >> bindings::PAGE_SECTORS_SHIFT;
+
+        let mut guard = tree.lock();
+        let mut page = if let Some(page) = guard.get_mut(idx) {
+            page
+        } else {
+            guard.store(
+                idx,
+                KBox::new(Page::alloc_page(flags::GFP_KERNEL)?, flags::GFP_KERNEL)?,
+                flags::GFP_KERNEL,
+            )?;
+            guard.get_mut(idx).unwrap()
+        };
+
+        segment.copy_to_page(&mut page)?;
+
+        Ok(())
+    }
+
+    #[inline(always)]
+    fn read(tree: TreeRef<'_>, sector: usize, segment: &mut Segment<'_>) -> Result {
+        let idx = sector >> bindings::PAGE_SECTORS_SHIFT;
+
+        let guard = tree.lock();
+        if let Some(page) = guard.get(idx) {
+            segment.copy_from_page(page)?;
+        }
+
+        Ok(())
+    }
+
+    #[inline(never)]
+    fn transfer(
+        command: bindings::req_op,
+        tree: TreeRef<'_>,
+        sector: usize,
+        segment: &mut Segment<'_>,
+    ) -> Result {
+        match command {
+            bindings::req_op_REQ_OP_WRITE => Self::write(tree, sector, segment)?,
+            bindings::req_op_REQ_OP_READ => Self::read(tree, sector, segment)?,
+            _ => (),
+        }
+        Ok(())
+    }
 }
 
+type Tree = XArray<KBox<Page>>;
+type TreeRef<'a> = &'a Tree;
+
+#[pin_data]
+struct TreeContainer {
+    // `XArray` is safe to use without a lock, as it applies internal locking.
+    // However, there are two reasons to use an external lock: a) cache line
+    // contention and b) we don't want to take the lock for each page we
+    // process.
+    //
+    // A: The `XArray` lock (xa_lock) is located on the same cache line as the
+    // xarray data pointer (xa_head). The effect of this arrangement is that
+    // under heavy contention, we often get a cache miss when we try to follow
+    // the data pointer after acquiring the lock. We would rather have consumers
+    // spinning on another lock, so we do not get a miss on xa_head. This issue
+    // can potentially be fixed by padding the C `struct xarray`.
+    //
+    // B: The current `XArray` Rust API requires that we take the `xa_lock` for
+    // each `XArray` operation. This is very inefficient when the lock is
+    // contended and we have many operations to perform. Eventually we should
+    // update the `XArray` API to allow multiple tree operations under a single
+    // lock acquisition. For now, serialize tree access with an external lock.
+    #[pin]
+    tree: CacheAligned<Tree>,
+    #[pin]
+    lock: CacheAligned<SpinLock<()>>,
+}
+
+impl TreeContainer {
+    fn new() -> impl PinInit<Self> {
+        pin_init!(TreeContainer {
+            tree <- CacheAligned::new_initializer(XArray::new(kernel::xarray::AllocKind::Alloc)),
+            lock <- CacheAligned::new_initializer(new_spinlock!((), "rnullb:mem")),
+        })
+    }
+}
+
+#[pin_data]
 struct QueueData {
+    #[pin]
+    tree: TreeContainer,
     irq_mode: IRQMode,
     completion_time: Ktime,
+    memory_backed: bool,
 }
 
 #[pin_data]
@@ -164,7 +271,7 @@ kernel::impl_has_hr_timer! {
 
 #[vtable]
 impl Operations for NullBlkDevice {
-    type QueueData = KBox<QueueData>;
+    type QueueData = Pin<KBox<QueueData>>;
     type RequestData = Pdu;
 
     fn new_request_data() -> impl PinInit<Self::RequestData> {
@@ -174,7 +281,26 @@ impl Operations for NullBlkDevice {
     }
 
     #[inline(always)]
-    fn queue_rq(queue_data: &QueueData, rq: ARef<mq::Request<Self>>, _is_last: bool) -> Result {
+    fn queue_rq(
+        queue_data: Pin<&QueueData>,
+        rq: ARef<mq::Request<Self>>,
+        _is_last: bool,
+    ) -> Result {
+        if queue_data.memory_backed {
+            let guard = queue_data.tree.lock.lock();
+            let tree = queue_data.tree.tree.deref();
+
+            let mut sector = rq.sector();
+            for bio in rq.bio_iter() {
+                for mut segment in bio.segment_iter() {
+                    Self::transfer(rq.command(), tree, sector, &mut segment)?;
+                    sector += segment.len() >> bindings::SECTOR_SHIFT;
+                }
+            }
+
+            drop(guard);
+        }
+
         match queue_data.irq_mode {
             IRQMode::None => mq::Request::end_ok(rq)
                 .map_err(|_e| kernel::error::code::EIO)
@@ -187,7 +313,7 @@ impl Operations for NullBlkDevice {
         Ok(())
     }
 
-    fn commit_rqs(_queue_data: &QueueData) {}
+    fn commit_rqs(_queue_data: Pin<&QueueData>) {}
 
     fn complete(rq: ARef<mq::Request<Self>>) {
         mq::Request::end_ok(rq)

metaspace/linux

drivers/block/rnull/configfs.rs
@@ -37,7 +37,7 @@ impl AttributeOperations<0> for Config {
 
     fn show(_this: &Config, page: &mut [u8; PAGE_SIZE]) -> Result<usize> {
         let mut writer = kernel::str::Formatter::new(page);
-        writer.write_str("blocksize,size,rotational,irqmode,completion_nsec\n")?;
+        writer.write_str("blocksize,size,rotational,irqmode,completion_nsec,memory_backed\n")?;
         Ok(writer.bytes_written())
     }
 }
@@ -61,6 +61,7 @@ impl configfs::GroupOperations for Config {
                 size: 3,
                 irqmode: 4,
                 completion_nsec: 5,
+                memory_backed: 6,
             ],
         };
 
@@ -78,6 +79,7 @@ impl configfs::GroupOperations for Config {
                     irq_mode: IRQMode::None,
                     completion_time: time::Delta::ZERO,
                     name: name.try_into()?,
+                    memory_backed: false,
                 }),
             }),
         ))
@@ -131,6 +133,7 @@ struct DeviceConfigInner {
     irq_mode: IRQMode,
     completion_time: time::Delta,
     disk: Option<GenDisk<NullBlkDevice>>,
+    memory_backed: bool,
 }
 
 #[vtable]
@@ -161,6 +164,7 @@ impl configfs::AttributeOperations<0> for DeviceConfig {
                 guard.capacity_mib,
                 guard.irq_mode,
                 guard.completion_time,
+                guard.memory_backed,
             )?);
             guard.powered = true;
         } else if guard.powered && !power_op {
@@ -292,6 +296,37 @@ impl configfs::AttributeOperations<5> for DeviceConfig {
         let completion_time: i64 = value.try_into()?;
 
         this.data.lock().completion_time = time::Delta::from_nanos(completion_time);
+        Ok(())
+    }
+}
+
+#[vtable]
+impl configfs::AttributeOperations<6> for DeviceConfig {
+    type Data = DeviceConfig;
+
+    fn show(this: &DeviceConfig, page: &mut [u8; PAGE_SIZE]) -> Result<usize> {
+        let mut writer = kernel::str::Formatter::new(page);
+
+        if this.data.lock().memory_backed {
+            writer.write_fmt(fmt!("1\n"))?;
+        } else {
+            writer.write_fmt(fmt!("0\n"))?;
+        }
+
+        Ok(writer.bytes_written())
+    }
+
+    fn store(this: &DeviceConfig, page: &[u8]) -> Result {
+        if this.data.lock().powered {
+            return Err(EBUSY);
+        }
+
+        this.data.lock().memory_backed = core::str::from_utf8(page)?
+            .trim()
+            .parse::<u8>()
+            .map_err(|_| kernel::error::code::EINVAL)?
+            != 0;
+
         Ok(())
     }
 }

drivers/block/rnull/rnull.rs
@@ -5,9 +5,12 @@
 mod configfs;
 
 use configfs::IRQMode;
+use core::ops::Deref;
 use kernel::{
+    bindings,
     block::{
         self,
+        bio::Segment,
         mq::{
             self,
             gen_disk::{self, GenDisk},
@@ -15,12 +18,17 @@ use kernel::{
         },
     },
     error::Result,
-    pr_info,
+    new_spinlock,
+    page::Page,
     prelude::*,
-    sync::Arc,
-    time::hrtimer::{HrTimerCallback, HrTimerPointer, HrTimerRestart},
-    time::Delta,
-    types::{ARef, OwnableRefCounted, Owned},
+    sync::{Arc, SpinLock},
+    time::{
+        hrtimer::{HrTimerCallback, HrTimerPointer, HrTimerRestart},
+        Delta,
+    },
+    types::{ARef, BorrowIterator, OwnableRefCounted, Owned},
+    xarray::{self, XArray},
+    CacheAligned,
 };
 use pin_init::PinInit;
 
@@ -58,14 +66,18 @@ impl NullBlkDevice {
         capacity_mib: u64,
         irq_mode: IRQMode,
         completion_time: Delta,
+        memory_backed: bool,
     ) -> Result<GenDisk<Self>> {
         let tagset = Arc::pin_init(TagSet::new(1, 256, 1, mq::Flags::default()), GFP_KERNEL)?;
 
-        let queue_data = Box::new(
+        let queue_data = Box::pin_init(
+            pin_init!(
             QueueData {
+                tree <- TreeContainer::new(),
                 irq_mode,
                 completion_time,
-            },
+                memory_backed,
+            }),
             GFP_KERNEL,
         )?;
 
@@ -76,11 +88,110 @@ impl NullBlkDevice {
             .rotational(rotational)
             .build(fmt!("{}", name.to_str()?), tagset, queue_data)
     }
+
+    #[inline(always)]
+    fn write(
+        tree: &mut xarray::Guard<'_, TreeNode>,
+        mut sector: usize,
+        mut segment: Segment<'_>,
+    ) -> Result {
+        while !segment.is_empty() {
+            let page_idx = sector >> block::PAGE_SECTORS_SHIFT;
+
+            let page = if let Some(page) = tree.get_mut(page_idx) {
+                page
+            } else {
+                let page = tree.do_unlocked(|| Page::alloc_page(GFP_NOIO))?;
+                tree.store(page_idx, page, GFP_NOIO)?;
+                tree.get_mut(page_idx).unwrap()
+            };
+
+            let page_offset = (sector & block::SECTOR_MASK as usize) << block::SECTOR_SHIFT;
+            sector += segment.copy_to_page(page, page_offset) >> block::SECTOR_SHIFT;
+        }
+        Ok(())
+    }
+
+    #[inline(always)]
+    fn read(
+        tree: &xarray::Guard<'_, TreeNode>,
+        mut sector: usize,
+        mut segment: Segment<'_>,
+    ) -> Result {
+        while !segment.is_empty() {
+            let idx = sector >> block::PAGE_SECTORS_SHIFT;
+
+            if let Some(page) = tree.get(idx) {
+                let page_offset = (sector & block::SECTOR_MASK as usize) << block::SECTOR_SHIFT;
+                sector += segment.copy_from_page(&page, page_offset) >> block::SECTOR_SHIFT;
+            } else {
+                sector += segment.zero_page() >> block::SECTOR_SHIFT;
+            }
+        }
+
+        Ok(())
+    }
+
+    #[inline(never)]
+    fn transfer(
+        command: bindings::req_op,
+        tree: &mut xarray::Guard<'_, TreeNode>,
+        sector: usize,
+        segment: Segment<'_>,
+    ) -> Result {
+        match command {
+            bindings::req_op_REQ_OP_WRITE => Self::write(tree, sector, segment)?,
+            bindings::req_op_REQ_OP_READ => Self::read(tree, sector, segment)?,
+            _ => (),
+        }
+        Ok(())
+    }
 }
 
+type TreeNode = Owned<Page>;
+type Tree = XArray<TreeNode>;
+
+#[pin_data]
+struct TreeContainer {
+    // `XArray` is safe to use without a lock, as it applies internal locking.
+    // However, there are two reasons to use an external lock: a) cache line
+    // contention and b) we don't want to take the lock for each page we
+    // process.
+    //
+    // A: The `XArray` lock (xa_lock) is located on the same cache line as the
+    // xarray data pointer (xa_head). The effect of this arrangement is that
+    // under heavy contention, we often get a cache miss when we try to follow
+    // the data pointer after acquiring the lock. We would rather have consumers
+    // spinning on another lock, so we do not get a miss on xa_head. This issue
+    // can potentially be fixed by padding the C `struct xarray`.
+    //
+    // B: The current `XArray` Rust API requires that we take the `xa_lock` for
+    // each `XArray` operation. This is very inefficient when the lock is
+    // contended and we have many operations to perform. Eventually we should
+    // update the `XArray` API to allow multiple tree operations under a single
+    // lock acquisition. For now, serialize tree access with an external lock.
+    #[pin]
+    tree: CacheAligned<Tree>,
+    #[pin]
+    lock: CacheAligned<SpinLock<()>>,
+}
+
+impl TreeContainer {
+    fn new() -> impl PinInit<Self> {
+        pin_init!(TreeContainer {
+            tree <- CacheAligned::new_initializer(XArray::new(kernel::xarray::AllocKind::Alloc)),
+            lock <- CacheAligned::new_initializer(new_spinlock!((), "rnullb:mem")),
+        })
+    }
+}
+
+#[pin_data]
 struct QueueData {
+    #[pin]
+    tree: TreeContainer,
     irq_mode: IRQMode,
     completion_time: Delta,
+    memory_backed: bool,
 }
 
 #[pin_data]
@@ -110,7 +221,7 @@ kernel::impl_has_hr_timer! {
 
 #[vtable]
 impl Operations for NullBlkDevice {
-    type QueueData = KBox<QueueData>;
+    type QueueData = Pin<KBox<QueueData>>;
     type RequestData = Pdu;
 
     fn new_request_data() -> impl PinInit<Self::RequestData> {
@@ -120,7 +231,32 @@ impl Operations for NullBlkDevice {
     }
 
     #[inline(always)]
-    fn queue_rq(queue_data: &QueueData, rq: Owned<mq::Request<Self>>, _is_last: bool) -> Result {
+    fn queue_rq(
+        queue_data: Pin<&QueueData>,
+        mut rq: Owned<mq::Request<Self>>,
+        _is_last: bool,
+    ) -> Result {
+        if queue_data.memory_backed {
+            //let guard = queue_data.tree.lock.lock();
+            let tree = queue_data.tree.tree.deref();
+            let command = rq.command();
+            let mut sector = rq.sector();
+            let mut guard = tree.lock();
+
+            for bio in rq.bio_iter_mut() {
+                let mut segment_iter = bio.segment_iter();
+                while let Some(segment) = segment_iter.next() {
+                    let length = segment.len();
+                    Self::transfer(command, &mut guard, sector, segment)?;
+                    sector += length as usize >> block::SECTOR_SHIFT;
+                }
+            }
+
+            drop(guard);
+
+            //drop(guard);
+        }
+
         match queue_data.irq_mode {
             IRQMode::None => rq.end_ok(),
             IRQMode::Soft => mq::Request::complete(rq.into()),
@@ -133,7 +269,7 @@ impl Operations for NullBlkDevice {
         Ok(())
     }
 
-    fn commit_rqs(_queue_data: &QueueData) {}
+    fn commit_rqs(_queue_data: Pin<&QueueData>) {}
 
     fn complete(rq: ARef<mq::Request<Self>>) {
         OwnableRefCounted::try_from_shared(rq)