c05aad0dc3c1b1e107272d5bac8b853aee03b0e1
Alice Ryhl <aliceryhl@google.com>
2026-01-05 12:42:40 +0000

    LIST: [PATCH v2 27/27] rust: xarray: add __rust_helper to helpers
    This is needed to inline these helpers into Rust code.
    
    Reviewed-by: Boqun Feng <boqun.feng@gmail.com>
    Reviewed-by: Gary Guo <gary@garyguo.net>
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Acked-by: Tamir Duberstein <tamird@gmail.com>
    Link: https://msgid.link/20260105-define-rust-helper-v2-27-51da5f454a67@google.com
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- rust/helpers/blk.c
+++ rust/helpers/blk.c
@@ -4,26 +4,27 @@
 #include <linux/blk-mq.h>
 #include <linux/blkdev.h>
 
-void *rust_helper_blk_mq_rq_to_pdu(struct request *rq)
+__rust_helper void *rust_helper_blk_mq_rq_to_pdu(struct request *rq)
 {
 	return blk_mq_rq_to_pdu(rq);
 }
 
-struct request *rust_helper_blk_mq_rq_from_pdu(void *pdu)
+__rust_helper struct request *rust_helper_blk_mq_rq_from_pdu(void *pdu)
 {
 	return blk_mq_rq_from_pdu(pdu);
 }
 
-void rust_helper_bio_advance_iter_single(const struct bio *bio,
-					 struct bvec_iter *iter,
-					 unsigned int bytes)
+__rust_helper void rust_helper_bio_advance_iter_single(const struct bio *bio,
+						       struct bvec_iter *iter,
+						       unsigned int bytes)
 {
 	bio_advance_iter_single(bio, iter, bytes);
 }
 
-bool rust_helper_blk_mq_add_to_batch(struct request *req,
-				     struct io_comp_batch *iob, bool is_error,
-				     void (*complete)(struct io_comp_batch *))
+__rust_helper bool
+rust_helper_blk_mq_add_to_batch(struct request *req, struct io_comp_batch *iob,
+				bool is_error,
+				void (*complete)(struct io_comp_batch *))
 {
 	return blk_mq_add_to_batch(req, iob, is_error, complete);
 }
--- rust/helpers/xarray.c
+++ rust/helpers/xarray.c
@@ -2,39 +2,44 @@
 
 #include <linux/xarray.h>
 
-int rust_helper_xa_err(void *entry)
+__rust_helper int rust_helper_xa_err(void *entry)
 {
 	return xa_err(entry);
 }
 
-int rust_helper_xa_trylock(struct xarray *xa)
+__rust_helper void rust_helper_xa_init_flags(struct xarray *xa, gfp_t flags)
+{
+	return xa_init_flags(xa, flags);
+}
+
+__rust_helper int rust_helper_xa_trylock(struct xarray *xa)
 {
 	return xa_trylock(xa);
 }
 
-void rust_helper_xa_lock(struct xarray *xa)
+__rust_helper void rust_helper_xa_lock(struct xarray *xa)
 {
 	return xa_lock(xa);
 }
 
-void rust_helper_xa_unlock(struct xarray *xa)
+__rust_helper void rust_helper_xa_unlock(struct xarray *xa)
 {
 	return xa_unlock(xa);
 }
 
-void *rust_helper_xas_result(struct xa_state *xas, void *curr)
+__rust_helper void *rust_helper_xas_result(struct xa_state *xas, void *curr)
 {
 	if (xa_err(xas->xa_node))
 		curr = xas->xa_node;
 	return curr;
 }
 
-void *rust_helper_xa_zero_to_null(void *entry)
+__rust_helper void *rust_helper_xa_zero_to_null(void *entry)
 {
 	return xa_is_zero(entry) ? NULL : entry;
 }
 
-int rust_helper_xas_error(const struct xa_state *xas)
+__rust_helper int rust_helper_xas_error(const struct xa_state *xas)
 {
 	return xas_error(xas);
 }
