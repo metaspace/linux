b49c0e5296d4ed2677b70ee8c700f77f3a467fd9
Benno Lossin <lossin@kernel.org>
2026-01-11 13:25:02 +0100

    LIST: [PATCH v2 4/15] rust: pin-init: internal: add utility API for syn error handling
    Add a function to turn a `syn::Result<TokenStream>` into a
    `TokenStream`, either containing the error or the normal stream.
    
    Add a nullable custom error type wrapping `syn::Error`.
    
    Signed-off-by: Benno Lossin <lossin@kernel.org>
    Link: https://lore.kernel.org/r/20260111122554.2662175-5-lossin@kernel.org
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- rust/pin-init/internal/src/lib.rs
+++ rust/pin-init/internal/src/lib.rs
@@ -36,3 +36,47 @@ pub fn derive_zeroable(input: TokenStream) -> TokenStream {
 pub fn maybe_derive_zeroable(input: TokenStream) -> TokenStream {
     zeroable::maybe_derive(input.into()).into()
 }
+
+#[expect(dead_code)]
+fn ok_or_compile_error(res: syn::Result<proc_macro2::TokenStream>) -> TokenStream {
+    match res {
+        Ok(stream) => stream,
+        Err(error) => error.into_compile_error(),
+    }
+    .into()
+}
+
+pub(crate) struct Error(Option<syn::Error>);
+
+impl From<syn::Error> for Error {
+    fn from(value: syn::Error) -> Self {
+        Self(Some(value))
+    }
+}
+
+impl Error {
+    #[expect(dead_code)]
+    pub(crate) fn none() -> Self {
+        Self(None)
+    }
+
+    #[expect(dead_code)]
+    pub(crate) fn combine(&mut self, error: impl Into<Self>) {
+        let error = error.into();
+        if let Some(this) = self.0.as_mut() {
+            if let Some(error) = error.0 {
+                this.combine(error);
+            }
+        } else {
+            self.0 = error.0;
+        }
+    }
+}
+
+impl quote::ToTokens for Error {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        if let Some(error) = self.0.as_ref() {
+            quote::ToTokens::to_tokens(&error.to_compile_error(), tokens);
+        }
+    }
+}
