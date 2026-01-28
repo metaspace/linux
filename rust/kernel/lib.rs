6917f2da56ebeb0529d1133a9e0e658cbc6ac42f
Andreas Hindborg <a.hindborg@kernel.org>
2026-01-28 15:05:20 +0100

    LIST: [PATCH] rust: add `CacheAligned` for easy cache line alignment of values
    `CacheAligned` allows to easily align values to a 64 byte boundary.
    
    An example use case is the kernel `struct spinlock`. This struct is 4 bytes
    on x86 when lockdep is not enabled. The structure is not padded to fit a
    cache line. The effect of this for `SpinLock` is that the lock variable and
    the value protected by the lock might share a cache line, depending on the
    alignment requirements of the protected value. Wrapping the value in
    `CacheAligned` to get a `SpinLock<CacheAligned<T>>` solves this problem.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@samsung.com>
    Link: https://lore.kernel.org/r/20260128-cache-aligned-v1-1-a4eb7c3ffd35@kernel.org
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -75,6 +75,7 @@ pub mod block;
 pub mod bug;
 #[doc(hidden)]
 pub mod build_assert;
+mod cache_aligned;
 pub mod clk;
 #[cfg(CONFIG_CONFIGFS_FS)]
 pub mod configfs;
@@ -159,6 +160,7 @@ pub mod xarray;
 
 #[doc(hidden)]
 pub use bindings;
+pub use cache_aligned::CacheAligned;
 pub use macros;
 pub use uapi;
 
