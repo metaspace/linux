bfa46087c268d1227c1a0252d8d44a8edeaac7fb
Andreas Hindborg <a.hindborg@kernel.org>
2026-02-11 15:18:33 +0100

    block: rust: fix generation of bindings to `BLK_STS_.*`
    Bindgen generates constants for CPP integer literals as u32. The
    `blk_status_t` type is defined as `u8` but the variants of the type are
    defined as integer literals via CPP macros. Thus the defined variants of
    the type are not of the same type as the type itself.
    
    Prevent bindgen from emitting generated bindings for the `BLK_STS_.*`
    defines and instead define constants manually in `bindings_helper.h`
    
    Also remove casts that are no longer necessary.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- rust/kernel/block/mq/operations.rs
+++ rust/kernel/block/mq/operations.rs
@@ -124,7 +124,7 @@ impl<T: Operations> OperationsVTable<T> {
         if let Err(e) = ret {
             e.to_blk_status()
         } else {
-            bindings::BLK_STS_OK as bindings::blk_status_t
+            bindings::BLK_STS_OK
         }
     }
 
