7b32454b02e60e5dc45ebd51bb199ebcee9d6104
Andreas Hindborg <a.hindborg@kernel.org>
2026-02-14 12:16:41 +0100

    block: rust: add polled completion support
    Add support for polled I/O completion to the Rust block layer bindings.
    This includes the `poll` callback in `Operations` and the
    `IoCompletionBatch` type for batched completions.
    
    The `poll` callback is invoked by the block layer when polling for
    completed requests on poll queues. Drivers can use `IoCompletionBatch`
    to batch multiple completions efficiently.
    
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- rust/kernel/block/mq.rs
+++ rust/kernel/block/mq.rs
@@ -89,7 +89,8 @@
 //!         _hw_data: (),
 //!         _queue_data: (),
 //!         rq: Owned<IdleRequest<Self>>,
-//!         _is_last: bool
+//!         _is_last: bool,
+//!         is_poll: bool
 //!     ) -> BlkResult {
 //!         rq.start().end_ok();
 //!         Ok(())
@@ -130,6 +131,7 @@ mod request;
 mod request_queue;
 pub mod tag_set;
 
+pub use operations::IoCompletionBatch;
 pub use operations::Operations;
 pub use request::Command;
 pub use request::IdleRequest;
