7123c8180bafe932d0a4d722ce34dfbeca0e5ea0
Andreas Hindborg <a.hindborg@kernel.org>
2026-02-15 21:10:16 +0100

    LIST: [PATCH 2/2] rust: add a wrapper for the `nr_online_nodes` C function
    This function returns the number of online numa nodes.
    
    Link: https://msgid.link/20260215-cpu-helpers-v1-2-fab13d817a5e@kernel.org
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -377,3 +377,14 @@ pub fn num_possible_cpus() -> u32 {
     // SAFETY: FFI call with no additional requirements.
     unsafe { bindings::num_possible_cpus() }
 }
+
+/// Returns the number of online numa nodes.
+pub fn num_online_nodes() -> u32 {
+    // NOTE: In some configurations, we can read this variable without an unsafe block.
+    // SAFETY: When numa is enabled, this is a global mutable static. We do as C and just read it,
+    // even though it might race.
+    #[allow(unused_unsafe)]
+    unsafe {
+        bindings::nr_online_nodes
+    }
+}
