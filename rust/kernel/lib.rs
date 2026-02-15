73249ac98e7949ab20691aece9cffe3a635d9b32
Andreas Hindborg <a.hindborg@kernel.org>
2026-02-15 21:10:15 +0100

    LIST: [PATCH 1/2] rust: add a wrapper for the `num_possible_cpus` C function
    This function returns the max number of CPUs that can be online.
    
    Link: https://msgid.link/20260215-cpu-helpers-v1-1-fab13d817a5e@kernel.org
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -371,3 +371,9 @@ pub fn file_from_location<'a>(loc: &'a core::panic::Location<'a>) -> &'a core::f
         c"<Location::file_as_c_str() not supported>"
     }
 }
+
+/// Returns maximum number of CPUs that may be online on the system.
+pub fn num_possible_cpus() -> u32 {
+    // SAFETY: FFI call with no additional requirements.
+    unsafe { bindings::num_possible_cpus() }
+}
